b2932ea46ae2644e881161f1de9570415c7cb976
caofan <caofan3611@163.com>
2026-01-19 05:34:36 +0000

    update js/index.js.
    Signed-off-by: caofan <15338350+caofan3611@user.noreply.gitee.com>

--- js/index.js
+++ js/index.js
@@ -1,11 +1,17 @@
-let a=document.createElement('h1');
-a.style.color='green';
-a.innerText='网站建设中';
-document.body.appendChild(a);
-setInterval(function(v){
-      if(v.innerText=='网站建设中......'){
-              v.innerText='网站建设中';
-      } else {
-              v.innerText+='.';
-      }
-},233,a);
+let main_h1=document.createElement('h1');
+let header=document.createElement('div');
+document.body.appendChild(header);
+header.style.width='auto';
+header.style.height='123px';
+header.style.border='2px solid skyblue'
+header.style.borderRadius='8px 8px 8px 8px'
+header.style.backgroundColor='skyblue';
+let tmp=document.createElement('h1');
+tmp.innerText='caofan的没用东西';
+tmp.style.fontSize='25px'
+tmp.style.textAlign='center';
+header.appendChild(tmp);
+tmp=document.createElement('a');
+tmp.href='badgames/count-of-number';
+tmp.innerText='没用的看数字有几个的游戏(我知道这边的css很简陋，但是我来不及了，明天再写吧)';
+document.body.appendChild(tmp);
